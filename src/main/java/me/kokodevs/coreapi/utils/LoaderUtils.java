ede4204e244c1b8733f95d572a2745431542bb80
KokoDevs <mineforest162@gmail.com>
2022-10-06 15:05:30 +0300

    added shortcuts

--- src/main/java/me/kokodevs/coreapi/utils/LoaderUtils.java
+++ src/main/java/me/kokodevs/coreapi/utils/LoaderUtils.java
@@ -2,6 +2,7 @@ package me.kokodevs.coreapi.utils;
 
 import org.bukkit.command.PluginCommand;
 import org.bukkit.event.Listener;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.reflections.Reflections;
 
@@ -9,30 +10,29 @@ import java.lang.reflect.InvocationTargetException;
 
 public class LoaderUtils {
 
-    public static void loadAllListeners(Class plugin, String listenersPackage) {
-        String packageName = plugin.getPackage().getName();
+    public static void loadAllListeners(Plugin plugin, String listenersPackage) {
+        String packageName = plugin.getClass().getPackage().getName();
 
         for (Class<?> clazz : new Reflections(packageName + "." + listenersPackage)
                 .getSubTypesOf(Listener.class)
         ) {
             try {
                 Listener listener = (Listener) clazz.getDeclaredConstructor().newInstance();
-                JavaPlugin javaPlugin = JavaPlugin.getProvidingPlugin(plugin);
-                javaPlugin.getServer().getPluginManager().registerEvents(listener, javaPlugin);
+                plugin.getServer().getPluginManager().registerEvents(listener, plugin);
             } catch (InstantiationException | IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
                 e.printStackTrace();
             }
         }
     }
-    public static void loadAllCommands(Class plugin, String commandsPackage) {
-        String packageName = plugin.getPackage().getName();
+    public static void loadAllCommands(Plugin plugin, String commandsPackage) {
+        String packageName = plugin.getClass().getPackage().getName();
 
         for (Class<?> clazz : new Reflections(packageName + "." + commandsPackage)
                 .getSubTypesOf(CommandUtils.class)
         ) {
             try {
                 CommandUtils pluginCommand = (CommandUtils) clazz.getDeclaredConstructor().newInstance();
-                JavaPlugin javaPlugin = JavaPlugin.getProvidingPlugin(plugin);
+                JavaPlugin javaPlugin = JavaPlugin.getProvidingPlugin(plugin.getClass());
                 javaPlugin.getCommand(pluginCommand.getCommandInfo().name()).setExecutor(pluginCommand);
             } catch (InstantiationException | IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
                 e.printStackTrace();
